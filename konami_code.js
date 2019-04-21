865dd32130640c4dd07de0e66370200a6c024395
Jason Wiram <jwiram@gmail.com>
2019-04-21 14:42:27 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -23,7 +23,7 @@ function init() {
       if (key === codes[index]) {
         index++;
         
-        if (index === codes.length) {
+        if (index === codes.length - 1) {
           alert ("Contra!");
           
           index = 0;
