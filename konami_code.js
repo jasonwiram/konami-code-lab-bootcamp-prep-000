c2cc34047bdb101ec8afbf343d77ddb9fe1d1e08
Jason Wiram <jwiram@gmail.com>
2019-04-21 14:50:17 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,22 +14,22 @@ const codes = [
 let index = 0
 
 function init() {
-  document.body.addEventListener('keydown', onKeyDownHandler)
+  document.body.addEventListener('keydown', onKeyDownHandler);
 }
 
 function onKeyDownHandler(e) {
-  const key = e.key
+  const key = e.key;
   
   if (key === codes[index]) {
     index++;
     
     if (index === codes.length) {
-      alert("Contra!")
-      index = 0
+      alert("Contra!");
+      index = 0;
     }
     
   } else {
-    index = 0
+    index = 0;
   }
   
 }
